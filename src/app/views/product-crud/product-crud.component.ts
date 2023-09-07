175588acfb818e2c5f861cbb6284b4ab63b2c78a
Thiago Saytson <thiagosaytson@poli.ufrj.br>
2023-09-07 06:25:41 -0300

    [feature] routes home and products

---
+++ src/app/views/product-crud/product-crud.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-product-crud',
+  templateUrl: './product-crud.component.html',
+  styleUrls: ['./product-crud.component.css']
+})
+export class ProductCrudComponent {
+  coolAtribute: string = "value";
+
+  doSomething(): void {
+    console.log('Doing something');
+  }
+}
